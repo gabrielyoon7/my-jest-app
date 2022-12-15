a03dfe5109469e992851d9a850ad040afa664f4f
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-12-15 15:21:47 +0900

    test: toStrictEqual

--- fn.test.js
+++ fn.test.js
@@ -17,11 +17,18 @@ test('이름과 나이를 전달받아서 객체를 반환해줘', () => {
         name: "Mike",
         age: 30,
     });
-})
+});
 
 test('이름과 나이를 전달받아서 객체를 반환해줘', () => {
     expect(fn.makeUser("Mike", 30)).toEqual({
         name: "Mike",
         age: 30,
     });
-})
+});
+
+test('이름과 나이를 전달받아서 객체를 반환해줘', () => {
+    expect(fn.makeUser("Mike", 30)).toStrictEqual({
+        name: "Mike",
+        age: 30,
+    });
+});
