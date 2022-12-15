3699fee171cae4ae72cdf92b07ce6845ce6b61a1
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-12-15 15:31:46 +0900

    test: 숫자 비교

--- fn.test.js
+++ fn.test.js
@@ -39,15 +39,34 @@ test('이름과 나이를 전달받아서 객체를 반환해줘', () => {
 
 test('null은 null 입니다.', () => {
     expect(null).toBeNull();
-})
+});
 
 // toBeTruthy
 // toBeFalsy
 
 test('0은 false 입니다.', () => {
     expect(fn.add(1, -1)).toBeFalsy();
-})
+});
 
 test('비어있지 않은 문자열을 true 입니다.', () => {
     expect(fn.add('hello', 'world')).toBeTruthy();
+});
+
+// toBeGreaterThan 크다
+// toBeGreaterThanOrEqual 크거나 같다
+// toBeLessThan 작다
+// toBeLessThanOrEqaul 작거나 같다
+
+test('ID는 10자 이하여야 합니다.', () => {
+    const id = 'GABRIELYOO'
+    expect(id.length).toBeLessThanOrEqual(10);
+});
+
+test('비밀번호는 4자리 입니다.', () => {
+    const pw = '1234'
+    expect(pw.length).toEqual(4);
+});
+
+test('0.1 더하기 0.2 는 0.3 입니다.', () => {
+    expect(fn.add(0.1, 0.2)).toBeCloseTo(0.3); //근사값
 })
