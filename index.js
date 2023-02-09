c2a14c5b0b5ffaf3d79222817f93f5f598c1e7e6
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-10 00:32:34 +0900

    feat: 입출력하는 기능 추가

---
+++ index.js
@@ -0,0 +1,8 @@
+const Console = require("./src/Console");
+
+const play = async () => {
+    const answer = await Console.read('무엇이든 입력해 보아요.')
+    Console.print(`당신이 입력한 값은 : ${answer}`)
+}
+
+play();
