3cd58c85284a805421697f01abd8bde03dabdee7
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-15 02:00:44 +0900

    feat: cli 선택기 예제 코드 추가

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-unused-vars */
+import cliSelect from 'cli-select';
+import chalk from 'chalk';
 import Console from './src/Console.js';
 
 const play = async () => {
@@ -5,4 +8,16 @@ const play = async () => {
   Console.print(`당신이 입력한 값은 : ${answer}`);
 };
 
+await cliSelect({
+  values: ['Major', 'Minor', 'Patch'],
+  valueRenderer: (value, selected) => {
+    if (selected) {
+      return chalk.underline(value);
+    }
+
+    return value;
+  },
+}).then((response) => {
+  Console.print(response);
+});
 play();
