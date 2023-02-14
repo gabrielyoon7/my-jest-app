d55b6f5bee413a915e1f2f97a966252dcb61559d
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-15 01:48:31 +0900

    style: eslint airbnb style로 수정

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import Console from "./src/Console.js";
+import Console from './src/Console';
 
 const play = async () => {
-    const answer = await Console.read('무엇이든 입력해 보아요.')
-    Console.print(`당신이 입력한 값은 : ${answer}`)
-}
+  const answer = await Console.read('무엇이든 입력해 보아요.');
+  Console.print(`당신이 입력한 값은 : ${answer}`);
+};
 
 play();
